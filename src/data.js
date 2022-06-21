8b13f7eb67f797f5ebfc6ce0fcd5aa4e866f41e2
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2022-06-21 13:53:34 +0100

    update

--- src/data.js
+++ src/data.js
@@ -26,4 +26,24 @@ export const nftCards = [
         img: nft5,
         title: "Relics Of A Mortal Past 5 - NFT",
     },
+    {
+        img: nft1,
+        title: "Relics Of A Mortal Past 5 - NFT",
+    },
+    {
+        img: nft2,
+        title: "Relics Of A Mortal Past 5 - NFT",
+    },
+    {
+        img: nft3,
+        title: "Relics Of A Mortal Past 5 - NFT",
+    },
+    {
+        img: nft4,
+        title: "Relics Of A Mortal Past 5 - NFT",
+    },
+    {
+        img: nft5,
+        title: "Relics Of A Mortal Past 5 - NFT",
+    },
 ]
